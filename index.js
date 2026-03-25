80d5029faaefd8f8fa7e72a083b2773e72766256
ajmalroshan123 <ajmalroshan2002@gmail.com>
2026-03-25 14:53:41 +0530

    feat: Added movie model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const env = require("dotenv");
 const mongoose = require("mongoose");
+const Movie = require("./models/movie.model")
 
 env.config();
 const app = express();
@@ -19,14 +20,35 @@ app.get("/", (req, res)=> {
     )
 })
 
-app.listen(process.env.PORT , ()=> {
 
-    console.log(`Server Started at ${process.env.PORT}`)
-    
-    mongoose.connect(process.env.DB_URL, ()=> {
-        console.log("Connected to mongo");
-    },
-    (err) => {
-        console.log('Not able to connect monge', err);
-    });
-});
+async function startServer() {
+    try {
+        // 1. Connect to DB
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Connected to MongoDB");
+
+        // 2. Start server AFTER DB connection
+        app.listen(process.env.PORT, () => {
+            console.log(`Server Started at ${process.env.PORT} `);
+        });
+
+        // // 3. Insert dummy data (optional)
+        // await Movie.create({
+        //     name: "Avatar",
+        //     description: "A marine on an alien planet becomes torn between two worlds.",
+        //     casts: ["Sam Worthington", "Zoe Saldana", "Sigourney Weaver"],
+        //     trailerUrl: "https://www.youtube.com/watch?v=5PSNL1qE6VY",
+        //     language: "English",
+        //     releaseDate: "2009-12-18",
+        //     director: "James Cameron",
+        //     releaseStatus: "RELEASED"
+        // });
+
+        // console.log("Dummy movie inserted !!");
+
+    } catch (err) {
+        console.log("Error starting server:", err);
+    }
+}
+
+startServer();
